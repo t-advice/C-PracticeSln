f0fc2e074d489376c7033ca7fe18a4eada224436
Tashwill Advice <tashwilladvice02@gmail.com>
2025-12-06 15:14:36 +0200

    comments added explaining code

--- C#Practice1/Program.cs
+++ C#Practice1/Program.cs
@@ -1,8 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.Write("Enter your name: ");
-string name = Console.ReadLine() ?? "World";
+string name = Console.ReadLine() ?? "please type your name";
 
 Console.Write("Enter your age:");
 int age = int.Parse(Console.ReadLine() ?? "0");
 
-Console.WriteLine($"Nice to meet you, {name}. You are {age} years old1 ");
+Console.WriteLine($"Nice to meet you, {name}. You are {age} years old ");
+
+// 2. prints message to screen without moving to a new line, wait for user.
+// 3.ReadLine() , reads whatever user types and returns the string, if null return text in q.marks.
+
+// 5. same as number 2.
+// 6. reads input as a string
+//    if null , 0 is used . int.Parse connverts string to int
+//8.  string interpolation, insert the variables
+
+//   --- ask name ----> save it-----> ask age --> covert--> print string interpolation.
