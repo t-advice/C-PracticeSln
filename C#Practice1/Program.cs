94ab1b73da3ecb2c5ff7b50ecef133178f2f1280
Tashwill Advice <tashwilladvice02@gmail.com>
2025-12-06 14:55:17 +0200

    Add project files.

---
+++ C#Practice1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter your name: ");
+string name = Console.ReadLine() ?? "World";
+
+Console.Write("Enter your age:");
+int age = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine($"Nice to meet you, {name}. You are {age} years old1 ");
